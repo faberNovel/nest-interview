45e3b8196b27ed100004313c91e400259d5029f0
Florian Nguyen <florian.nguyen@fabernovel.ey.com>
2025-03-25 11:12:31 +0100

    chore(): add stats type and service tests

--- src/pokemon/types/pokemon.ts
+++ src/pokemon/types/pokemon.ts
@@ -1,5 +1,14 @@
 import { PokemonSpecies } from './species';
 
+export type PokemonStats = {
+  hp: number;
+  attack: number;
+  defense: number;
+  specialAttack: number;
+  specialDefense: number;
+  speed: number;
+};
+
 export type Pokemon = {
   name: string;
   id: number;
@@ -7,4 +16,9 @@ export type Pokemon = {
   weight: number;
   types: string[];
   species: PokemonSpecies;
+  stats: PokemonStats;
 };
+
+export type PokemonWithStats = {
+  same_type_average_stats: PokemonStats;
+} & Pokemon;
