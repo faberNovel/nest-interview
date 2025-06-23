6ea73d0536de4be9e1daeaa8986653c4217c2c7f
Michael ANTOUN <michael.antoun@ext.hermes.com>
2025-06-23 15:12:30 +0200

    chore: add validation pipe

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import * as request from 'supertest';
 
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
 
 describe('NestJS app (e2e)', () => {
@@ -14,6 +14,8 @@ describe('NestJS app (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+
     await app.init();
   });
 
